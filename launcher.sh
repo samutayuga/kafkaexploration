5847860bbb5dc1a0f08855e1200aa00a9751539d
Putu Mas Mertayasa <putumas.mertayasa.e@thalesdigital.io>
2021-05-26 14:59:50 +0800

    cleanup

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,2 @@
-export APP_ETC=/Users/putumasm/IdeaProjects/kafkaexploration
-java -Dlogback.configurationfile=target/classes/logback.xml -classpath target/kafka-twitter-1.0.0-fat.jar com.samutup.explore.TweetyMaster
+export APP_ETC=/Users/putumasm/IdeaProjects/kafkaexploration/docker
+java -Dlogback.configurationfile=target/classes/logback.xml -classpath target/kafka-twitter-fat.jar com.samutup.explore.TweetyMaster
